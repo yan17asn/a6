ff4863ea05fbcded849739a88f3182a4fe2da4d3
yan17asn <248764378@qq.com>
2020-02-25 19:29:46 -0800

    Add files via upload

--- survey_view.php
+++ survey_view.php
@@ -22,12 +22,16 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 }
 
 //sql statement to select individual item
-$sql = "select Title,Description,DateAdded from winter2020_surveys where SurveyID = " . $myID;
+//$sql = "select Title,Description,DateAdded from winter2020_surveys where SurveyID = " . $myID;
 //---end config area --------------------------------------------------
 
 $foundRecord = FALSE; # Will change to true, if record found!
    
 # connection comes first in mysqli (improved) function
+$mySurvey = new Survey($myID);
+
+dumpDie($mySurvey);
+/*
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
 if(mysqli_num_rows($result) > 0)
@@ -42,23 +46,12 @@ if(mysqli_num_rows($result) > 0)
 }
 
 @mysqli_free_result($result); # We're done with the data!
+*/
 
 if($foundRecord)
 {#only load data if record found
 	$config->titleTag = $Title . "survey"; #overwrite PageTitle with survey info!
 }
-/*
-$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
-$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
-$config->metaRobots = 'no index, no follow';
-$config->loadhead = ''; #load page specific JS
-$config->banner = ''; #goes inside header
-$config->copyright = ''; #goes inside footer
-$config->sidebar1 = ''; #goes inside left side of page
-$config->sidebar2 = ''; #goes inside right side of page
-$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
-$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
-*/
 # END CONFIG AREA ---------------------------------------------------------- 
 
 get_header(); #defaults to theme header or header_inc.php
@@ -82,4 +75,66 @@ if($foundRecord)
 }
 
 get_footer(); #defaults to theme footer or footer_inc.php
-?>
+
+class Survey{
+	public $SurveyID = 0;
+	public $Title = '';
+	public $Description = '';
+	public $DateAdded = '';
+	public $Question = array();
+
+    public function __construct($id){
+		$this->SurveyID = (int)$id;
+		
+		$sql = "select Title,Description,DateAdded from winter2020_surveys where SurveyID = " . $this->SurveyID;
+
+		$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+        if(mysqli_num_rows($result) > 0)
+        {#records exist - process
+	        $foundRecord = TRUE;	
+	        while ($row = mysqli_fetch_assoc($result))
+	        {
+			    $this->Title = dbOut($row['Title']);
+			    $this->Description = dbOut($row['Description']);
+			    $this->DateAdded = dbOut($row['DateAdded']);
+		    }
+        }
+
+		@mysqli_free_result($result); # We're done with the data!
+		
+		
+	$sql = "select QuestionID,Question,Description from winter2020_questions where SurveyID = " . $this->SurveyID;
+
+		$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+        if(mysqli_num_rows($result) > 0)
+        {#records exist - process	
+	        while ($row = mysqli_fetch_assoc($result))
+	        {
+				$this->Questions[] = new Question(dbOut($row['QuestionID']), dbOut($row['Question']), dbOut($row['Description']));
+			    //$this->Title = dbOut($row['Title']);
+			    //$this->Description = dbOut($row['Description']);
+			    //$this->DateAdded = dbOut($row['DateAdded']);
+		    }
+        }
+
+        @mysqli_free_result($result); # We're done with the data!
+	}//end of Survey constructor
+}//end of survey class
+
+class Question{
+	public $QuestionID = 0;
+	public $Question = '';
+	public $Description = '';
+	
+	public function __construct($QuestionID,$Question,$Description){
+		$this->QuestionID = $QuestionID;
+		$this->Question = $Question;
+		$this->Description = $Description;
+
+
+	}//end of Question constructor
+
+}//end of Question class
+
